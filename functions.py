4d756e2f7310cb8b0dd25b26f59679db6e4fdf4f
Carissa Illy <trackfinder@trackfinder.app>
2023-06-05 15:58:18 -0700

    Additional info on pip, environments, and anaconda

--- functions.py
+++ functions.py
@@ -13,8 +13,29 @@ def hello_func_with_default(greeting, name = 'You'):
 
 hello_func_with_param('Hi').upper()
 
-def student_info(*args, **kwargs):
+def student_info(*args, **kwargs): # args = tuple, kwarg = dictionary
     print(args)
     print(kwargs)
 
 student_info('Math', 'Art', name='John', age = 22)
+
+courses = ['Math', 'Art']
+info = {'name': 'Jonn', 'age': 22}
+
+student_info(*courses, **info)
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31,30,31,30,31]
+
+def is_leap(year):
+    return  year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+def days_in_month(year, month):
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 20
+
+    return month_days[month]
+
+print(days_in_month(2020, 2))
---
+++ virtualenv.py
@@ -0,0 +1,11 @@
+# Seperate different python environments for different projects
+# Ensures that package collisions don't occur across projects
+
+# Creating a virutal environment
+# Cd to environment directory or create one
+# virtualenv PROJECT_NAME
+# source PROJECT_NAME/bin/activate
+# which python (shows which environment)
+# deactivate (get out of current python environment)
+# rm -rm PROJECT_NAME
+# virtualenv -p /usr/bin/python2.6 PROJECT_NAME (create environment with certain python version)
